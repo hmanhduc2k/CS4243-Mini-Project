7085909af674642fba3ad20a38181b3deefc2aea
Thang Pham <bathang02@gmail.com>
2022-11-04 18:01:03 +0800

    Change dataset from CIFAR to cs4243

--- initData.sh
+++ initData.sh
@@ -4,7 +4,7 @@ typeset NORMAL_TEMP_LABELLED=$( mktemp )
 touch "${NORMAL_TEMP_LABELLED}"
 
 ls cs4243_smallest/normal > "${NORMAL_TEMP}"
-awk '{print "normal/"$0", 0"}' "${NORMAL_TEMP}" > "${NORMAL_TEMP_LABELLED}"
+awk '{print "normal/"$0",0"}' "${NORMAL_TEMP}" > "${NORMAL_TEMP_LABELLED}"
 
 typeset CARRYING_TEMP=$( mktemp )
 touch "${CARRYING_TEMP}"
@@ -12,7 +12,7 @@ typeset CARRYING_TEMP_LABELLED=$( mktemp )
 touch "${CARRYING_TEMP_LABELLED}"
 
 ls cs4243_smallest/carrying > "${CARRYING_TEMP}"
-awk '{print "carrying/"$0", 1"}' "${CARRYING_TEMP}" > "${CARRYING_TEMP_LABELLED}"
+awk '{print "carrying/"$0",1"}' "${CARRYING_TEMP}" > "${CARRYING_TEMP_LABELLED}"
 
 typeset THREAT_TEMP=$( mktemp )
 touch "${THREAT_TEMP}"
@@ -20,7 +20,7 @@ typeset THREAT_TEMP_LABELLED=$( mktemp )
 touch "${THREAT_TEMP_LABELLED}"
 
 ls cs4243_smallest/threat > "${THREAT_TEMP}"
-awk '{print "threat/"$0", 2"}' "${THREAT_TEMP}" > "${THREAT_TEMP_LABELLED}"
+awk '{print "threat/"$0",2"}' "${THREAT_TEMP}" > "${THREAT_TEMP_LABELLED}"
 
-# echo "directory, label" > dataset.csv
+echo "directory,label" > dataset.csv
 cat "${NORMAL_TEMP_LABELLED}" "${CARRYING_TEMP_LABELLED}" "${THREAT_TEMP_LABELLED}" > dataset.csv
--- splitTrainTest.sh
+++ splitTrainTest.sh
@@ -1,4 +1,4 @@
 
-echo "directory, label" > train_label.csv
-echo "directory, label" > test_label.csv
-awk '{if(rand()<0.9) {print $0 > "train_label.csv"} else {print $0 > "test_label.csv"}}' dataset.csv
+echo "directory,label" > train_label.csv
+echo "directory,label" > test_label.csv
+awk -v seed=999 'BEGIN{srand(seed);}(NR>1){if(rand()<0.9) {print $0 >> "train_label.csv"} else {print $0 >> "test_label.csv"}}' dataset.csv
